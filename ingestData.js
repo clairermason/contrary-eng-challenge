9db9484178e913663f7c73ce5d453db3cf9b5d34
Claire Mason <clairemason@Claires-MBP.home>
2022-11-24 11:59:34 -0800

    connecting to heroku postgres server

--- ingestData.js
+++ ingestData.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
-const csv = require('csvtojson')
+const csv = require('csvtojson');
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const bucketName = 'contrary-engineering-interview';
 const peopleKey = 'data/people.csv';
@@ -16,7 +17,25 @@ const retrieveCSV = async (key) => {
     }    
 }
 
-// this function should fetch the data and insert into postgres
+
+// I have a postgres server up through my heroku app, so I'm gonna try to just write to that since I will need data there anyways
+
+const path = 'postgres://tfnzbtpwrrrbqj:bf7877ca7a6e96acbb489159d6f2ba4a5dcda81be912b435c58b1c01a92bb573@ec2-44-207-253-50.compute-1.amazonaws.com:5432/d2nqbha0vl0ohd';
+const sequelize = new Sequelize(path, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+});  
+sequelize.authenticate().then(() => {
+    console.log("Success!");
+    }).catch((err) => {
+    console.log(err);
+    });
+
 const fetchData = async () => {
     const peopleDataJSON = await retrieveCSV(peopleKey);
     // console.log(peopleDataJSON);
