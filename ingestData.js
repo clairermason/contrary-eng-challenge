e0548a8ef6a3907be6c5994161da2a6cd29a55c6
Claire Mason <clairemason@Claires-MBP.home>
2022-11-23 17:28:53 -0800

    accessing data and converting to JSON

--- ingestData.js
+++ ingestData.js
@@ -1,30 +1,27 @@
-// const { S3Client, AbortMultipartUploadCommand } = require("@aws-sdk/client-s3");
-const { S3 } = require("@aws-sdk/client-s3");
-const AWS = require("@aws-sdk/client-s3");
+const AWS = require("aws-sdk");
+const csv = require('csvtojson')
 
-// const peoplePath = 'https://contrary-engineering-interview.s3.amazonaws.com/data/people.csv'
-//  const companiesPath = 'https://contrary-engineering-interview.s3.amazonaws.com/data/companies.csv'
 const bucketName = 'contrary-engineering-interview';
 const peopleKey = 'data/people.csv';
 const companiesKey = 'data/companies.csv';
-const client = new AWS.S3();
 
-
-const fetchData = async (key) => {
+const S3 = new AWS.S3();
+const retrieveCSV = async (key) => {
     try {
-        const data = await client.getObject({Bucket: bucketName, Key: key});
-        console.log(data.Body);
+        const stream = S3.getObject({Bucket: bucketName, Key: key}).createReadStream();
+        const json = await csv().fromStream(stream);
+        return json;
     } catch (error){
         console.log('error!')
     }    
 }
-// this is currently console logging metadata, so it is correctly accessing AWS!
-// committing here so I have a record
-fetchData(peopleKey);
-fetchData(companiesKey);
-// console.log(peoplePath);
 
+// this function should fetch the data and insert into postgres
+const fetchData = async () => {
+    const peopleDataJSON = await retrieveCSV(peopleKey);
+    // console.log(peopleDataJSON);
+    const companiesDataJSON = await retrieveCSV(companiesKey);
+    // console.log(companiesDataJSON);
+};
 
-
-
-
+fetchData();
