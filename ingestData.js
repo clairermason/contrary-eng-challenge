d2e3e3317f2e01a4732c0c95291e9798b18b21dd
Claire Mason <clairemason@Claires-MacBook-Pro.local>
2022-11-26 13:08:36 -0800

    finalize script to insert data into postgres

--- ingestData.js
+++ ingestData.js
@@ -1,7 +1,6 @@
 const AWS = require("aws-sdk");
-const { assert } = require("console");
 const csv = require('csvtojson');
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const bucketName = 'contrary-engineering-interview';
 const peopleKey = 'data/people.csv';
@@ -29,7 +28,8 @@ const sequelize = new Sequelize(path, {
           require: true,
           rejectUnauthorized: false
         }
-      }
+      },
+    logging: false
 }); 
 const authenticate = async () => {
     await sequelize.authenticate().then(() => {
@@ -39,16 +39,14 @@ const authenticate = async () => {
     });    
 }
 
-// since I need to define schema and PK/FK, this function is going to be fairly manual
+// since I need to define schema, data insertion is going to be fairly manual
+// it looks like PERSON_ID is not unique 
+// I'm going to keep those dupes for now and delete them later if they look like errors
 // the sequelize.define method has built in CREATE TABLE IF NOT EXISTS
-const createTables = async () => {
+const createPeopleTable = async () => {
     await authenticate();
     sequelize.define('people', {
-        PERSON_ID: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
+        PERSON_ID: DataTypes.STRING,
         COMPANY_NAME: DataTypes.STRING,
         COMPANY_LI_NAME: DataTypes.STRING,
         LAST_TITLE: DataTypes.STRING,
@@ -56,32 +54,72 @@ const createTables = async () => {
         GROUP_END_DATE: DataTypes.DATE
     }).sync(); 
 
+    const peopleModel = sequelize.model('people')
+    const dataJSON = await retrieveCSV(peopleKey);
+    console.log(`inserting ${dataJSON.length} rows`)
+    // total rows 5391 - checked in psql that all are there
+    for(let i=0; i < dataJSON.length; i++) {
+        peopleModel.create({
+            PERSON_ID: dataJSON[i].PERSON_ID,
+            COMPANY_NAME: dataJSON[i].COMPANY_NAME,
+            COMPANY_LI_NAME: dataJSON[i].COMPANY_LI_NAME,
+            LAST_TITLE: dataJSON[i].LAST_TITLE,
+            GROUP_START_DATE: dataJSON[i].GROUP_START_DATE || null,
+            GROUP_END_DATE: dataJSON[i].GROUP_END_DATE || null
+        });
+    }
+    console.log('rows inserted')
+}
+
+createPeopleTable();
+
+// seeing some dupes on NAME, so I'm just gonna allow them for now so I can get all data into the db
+const createCompanyTable = async () => {
     sequelize.define('companies', {
-        NAME: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
+        NAME: DataTypes.STRING,
         COMPANY_LINKEDIN_NAMES: DataTypes.ARRAY(DataTypes.STRING),
-        DESCRIPTION: DataTypes.STRING,
-        HEADCOUNT: DataTypes.INTEGER,
+        DESCRIPTION: DataTypes.STRING(10000),
+        HEADCOUNT: DataTypes.BIGINT,
         FOUNDING_DATE: DataTypes.DATE,
-        MOST_RECENT_RAISE: DataTypes.DATE,
-        MOST_RECENT_VALUATION: DataTypes.INTEGER,
+        MOST_RECENT_RAISE:  DataTypes.BIGINT,
+        MOST_RECENT_VALUATION: DataTypes.BIGINT,
         INVESTORS: DataTypes.ARRAY(DataTypes.STRING),
-        KNOWN_TOTAL_FUNDING: DataTypes.INTEGER,
+        KNOWN_TOTAL_FUNDING: DataTypes.BIGINT,
     }).sync();
     
-    const people = sequelize.model('people')
+    
     const companies = sequelize.model('companies')
-    // COMPANY_NAME is the foreign key linking to companies model, need to define that
-}
-createTables();
-// tables are not being created
+    const dataJSON = await retrieveCSV(companiesKey);
+    console.log(`inserting ${dataJSON.length} rows`)
 
-
-const insertData = async (key) => {
-    const dataJSON = await retrieveCSV(key);
+    // total rows 10711 - checked in psql that all are there
+    // im getting timeouts so break up the JSON into chunks
+    const totalLength = dataJSON.length
+    let lengthArray = []
+    for(let x = 0; x <= totalLength; x = x+2000){
+        lengthArray.push(x)
+    }
+    lengthArray.push(totalLength);
+    for(let j = 0; j < lengthArray.length; j++){
+        let beginIndex = lengthArray[j];
+        let endIndex = lengthArray[j+1]
+        for(let i = beginIndex; i < endIndex; i++){
+            await companies.create({
+            NAME: dataJSON[i].NAME,
+            COMPANY_LINKEDIN_NAMES: JSON.parse(dataJSON[i].COMPANY_LINKEDIN_NAMES),
+            DESCRIPTION: dataJSON[i].DESCRIPTION,
+            HEADCOUNT: dataJSON[i].HEADCOUNT || null,
+            FOUNDING_DATE: dataJSON[i].FOUNDING_DATE || null,
+            MOST_RECENT_RAISE: dataJSON[i].MOST_RECENT_RAISE || null,
+            MOST_RECENT_VALUATION: dataJSON[i].MOST_RECENT_VALUATION || null,
+            INVESTORS: JSON.parse(dataJSON[i].INVESTORS || null),
+            KNOWN_TOTAL_FUNDING: dataJSON[i].KNOWN_TOTAL_FUNDING || null
+        });        
+        }
+    }
+    
+    console.log('rows inserted')
 };
 
-insertData(peopleKey);
+createCompanyTable();
+console.log('yay! all data inserted!')
