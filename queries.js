6ab62d04a851ab4118533da19052881c5583740b
Claire Mason <clairemason@Claires-MacBook-Pro.local>
2022-11-29 10:05:07 -0800

    initial api routes

---
+++ queries.js
@@ -0,0 +1,22 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'tfnzbtpwrrrbqj',
+  host: 'ec2-44-207-253-50.compute-1.amazonaws.com',
+  database: 'd2nqbha0vl0ohd',
+  password: 'bf7877ca7a6e96acbb489159d6f2ba4a5dcda81be912b435c58b1c01a92bb573',
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM people LIMIT 10', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getUsers
+  }
